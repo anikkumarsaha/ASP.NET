838505b041b0a7053b3b8cd9ff2cb4db137d43f8
anikkumarsaha <anikkumarofficial08@gmail.com>
2022-11-12 18:44:17 +0600

    First API application

---
+++ FirstAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        [Route("api/dept/info")]
+        [HttpGet]
+        public HttpResponseMessage GetInfo()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Info from custom route");
+        }
+        [Route("api/dept/All")]
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Getting All from custom route");
+        }
+    }
+}
---
+++ FirstAPI/Controllers/FirstAPIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstAPI.Controllers
+{
+    public class FirstAPIController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var data = new { Id = 1, Name = "Anik" };
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        public HttpResponseMessage Post()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "This is a post request");
+        }
+        public HttpResponseMessage Delete()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "This is delete request");
+        }
+    }
+}
